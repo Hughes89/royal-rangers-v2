4a670163bf68a5f1d0e9f4d392142403c02fbd47
Hughes89 <mik3h89@gmail.com>
2018-11-06 22:25:59 -0500

    update classes and row count

--- src/Events/AddEvent/Forms/TextAreaInput.js
+++ src/Events/AddEvent/Forms/TextAreaInput.js
@@ -5,13 +5,14 @@ const TextAreaInput = ({ name, label, value, valid, onChange, placeholder, help 
         <label htmlFor={name} className="label">{label}</label>
         <div className="control">
             <textarea  
-                className={valid ? 'input is-success' : 'input is-danger'}
+                className={valid ? 'textarea is-success' : 'textarea is-danger'}
                 id={name}
                 type="text"
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
                 name={name}
+                rows="3"
             />
         </div>
         <p className="help">{help}</p>
